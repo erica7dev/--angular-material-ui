bae94b3c862619f190b9a9bcf6ce16a7e119d5c1
erica7monteiro@gmail.com <erikaisa7dev@gmail.com>
2022-05-26 23:41:57 -0300

    falta conectar api

---
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject, OnInit } from '@angular/core'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
+import { PeriodicElement } from 'src/app/models/PeriodicElement'
+
+@Component({
+  selector: 'app-element-dialog',
+  templateUrl: './element-dialog.component.html',
+  styleUrls: ['./element-dialog.component.scss']
+})
+export class ElementDialogComponent implements OnInit {
+  element!: PeriodicElement
+  isChange!: boolean
+
+  constructor (
+    @Inject(MAT_DIALOG_DATA)
+    public data: PeriodicElement,
+    public dialogRef: MatDialogRef<ElementDialogComponent>
+  ) { }
+
+  ngOnInit (): void {
+  }
+
+  onCancel (): void {
+    this.dialogRef.close()
+  }
+}
